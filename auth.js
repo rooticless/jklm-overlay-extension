29d95b420731565b71cfd383c0be8ccdde32bf6d
rooticles <kitroot570@gmail.com>
2026-07-13 19:26:08 +0200

    Delete auth.js

--- auth.js
+++
@@ -1,8 +0,0 @@
-// Authentication has been replaced with a local generated user ID flow.
-// This file intentionally provides no email/password login logic.
-
-async function getCurrentUserId() {
-    return new Promise((resolve) => {
-        chrome.storage.local.get(['userId'], (result) => resolve(result.userId || null));
-    });
-}
